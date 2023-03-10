6ec2d728f3767a40d039fe24ee8b3092b523c709
Hussein Mohammadi <mohammadi.itech@gmail.com>
2023-03-11 00:09:32 +0330

    build menu items and fix routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+
   title = 'نرم افزار حسابداری';
+  pageTitle: string = ''
+
+  constructor( private titleService : Title) {}
+    
+  ngOnInit(): void {
+    this.pageTitle = this.titleService.getTitle();
+  }
+
+  ngDoCheck() {
+    this.pageTitle = this.titleService.getTitle();
+  }
 }
